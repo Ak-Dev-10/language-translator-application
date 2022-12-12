1c11f71ab1a3d382239d7b420555d066e23e30e2
Akash Pande <akashpande1005@gmail.com>
2022-12-12 13:30:39 +0530

    new commit
    added complete styling of the page

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input ");
+var textOutput = document.querySelector("#output");
+
+var serverURL = 
 
 console.log(textInput);
 
-function clickHandler(){
-    console.log("clicked");
-    console.log("input",textInput.value);
-
+function getTranslationURL(text){
+    return serverURL+"";
 }
 btnTranslate.addEventListener("click",clickHandler);
